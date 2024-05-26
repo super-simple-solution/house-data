03df0fdb7865b6096390c465aa30d10f7ec46711
zhangpanrobot <zhangpanrobot@gmail.com>
2024-05-26 18:25:44 +0800

    add tax price

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -20,9 +20,14 @@ function init() {
   const totalArea = getEle('.mainInfo', areaEle)
   if (!priceEle || !areaEle) return
   const totalPriceEle = getEle('.total', priceEle)
-  const unitPrice = Math.ceil((Number(totalPriceEle?.textContent) * 10000) / areaInfo.size)
+  const housePrice = Number(totalPriceEle?.textContent)
+  const unitPrice = Math.ceil((housePrice * 10000) / areaInfo.size)
   const taxElTarget = getEle('.jing-item + .item')
   const tax = taxElTarget?.querySelector('.number')?.textContent
+  const taxNumber = getNumber(tax)
+  const agencyFee = [housePrice * 0.01, housePrice * 0.03]
+  const totalPriceLow = (housePrice + taxNumber + agencyFee[0]).toFixed(2)
+  const totalPriceHigh = (housePrice + taxNumber + agencyFee[1]).toFixed(2)
 
   // 跳转至测量/套内面积数据
   const goToAreaEle = () => {
@@ -47,30 +52,50 @@ function init() {
   }
 
   const areaTarget = () => (
-    <a class="sss-text-primary sss-cursor-pointer" onClick={goToAreaEle}>
+    <a class="sss-cursor-pointer sss-text-primary" onClick={goToAreaEle}>
       {areaInfo.type === AreaType.Manual ? '测量面积' : '套内面积'}
     </a>
   )
 
   const taxTarget = () => (
-    <a class="sss-text-primary sss-cursor-pointer" onClick={goToTaxEle}>
-      税费
+    <a class="" onClick={goToTaxEle}>
+      {tax}
     </a>
   )
 
   const taxEle = (
-    <span>
-      税费：
-      <span class="sss-text-warning sss-hover:cursor-pointer sss-hover:underline">{tax}万元</span>
-      <span>{taxTarget()}</span>
-    </span>
+    <div class="sss-my-1">
+      <div class="sss-text-xs sss-text-info">
+        <span>
+          <span class="sss-cursor-pointer sss-text-lg sss-font-semibold sss-text-warning hover:sss-underline">
+            {taxTarget()}
+            <span class="sss-text-xs sss-font-normal">万</span>
+          </span>
+          税费
+        </span>
+        <span>
+          <span class="sss-text-lg sss-font-semibold sss-text-warning">
+            {agencyFee[0].toFixed(2)}~{agencyFee[1].toFixed(2)}
+            <span class="sss-text-xs sss-font-normal">万</span>
+          </span>
+          中介费
+        </span>
+      </div>
+      <div class="sss-my-1 sss-text-xs sss-text-info">
+        <span class="sss-text-lg sss-font-semibold sss-text-warning">
+          {totalPriceLow}~{totalPriceHigh}
+          <span class="sss-text-xs sss-font-normal">万</span>
+        </span>
+        总成本
+      </div>
+    </div>
   )
 
   // 测量/套内面积对应价格
   const finalPriceEle = (
     <div class="final-price-ele sss-my-1 sss-flex sss-items-center">
       <div class="sss-text-xs">
-        <span class="sss-text-warning sss-text-lg sss-font-semibold">{unitPrice}</span>元/平米
+        <span class="sss-text-lg sss-font-semibold sss-text-warning">{unitPrice}</span>元/平米
         <span class="sss-text-info">(根据{areaTarget()}计算)</span>
       </div>
     </div>
@@ -80,7 +105,7 @@ function init() {
   const finalAreaEle = (
     <div class="final-area-ele sss-my-1 sss-flex sss-w-[150px] sss-items-center">
       <div class="sss-text-xs">
-        <span class="sss-text-warning sss-text-lg sss-font-semibold">{areaInfo.size}</span>平米
+        <span class="sss-text-lg sss-font-semibold sss-text-warning">{areaInfo.size}</span>平米
         <span class="sss-text-info">({areaTarget()})</span>
       </div>
     </div>
@@ -92,8 +117,8 @@ function init() {
   const efficiencyRatioEle = (
     <div class="sss-flex sss-items-center">
       <div class="sss-text-xs">
+        <span class="sss-text-lg sss-font-semibold sss-text-warning">{efficiencyRatio}</span>
         <span>得房率</span>
-        <span class="sss-text-warning sss-text-lg sss-font-semibold"> {efficiencyRatio}</span>
         <span class="sss-text-info">(根据{areaTarget()}计算)</span>
       </div>
     </div>
@@ -155,5 +180,5 @@ function scrollAndBlink(scrollTarget: HTMLElement, blinkTarget: HTMLElement) {
   blinkTarget?.classList.add('alerts-border')
   setTimeout(() => {
     blinkTarget?.classList.remove('alerts-border')
-  }, 2000)
+  }, 3000)
 }
